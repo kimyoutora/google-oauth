d5d1edf59faeb37f13e49f490d23a3cd398d831e
Kang Chen <kang@twitter.com>
2012-06-21 22:50:13 -0700

    fixed some load paths

--- lib/google_oauth.rb
+++ lib/google_oauth.rb
@@ -8,9 +8,9 @@ module GoogleOAuth
     end
   end
 
-  autoload :Calendar, "./google_oauth/calendar"
-  autoload :Client, "./google_oauth/client"
-  autoload :Document, "./google_oauth/document"
-  autoload :Contancts, "./google_oauth/contacts"
-  autoload :Mail, "./google_oauth/mail"
+  autoload :Calendar, "google_oauth/calendar"
+  autoload :Client, "google_oauth/client"
+  autoload :Document, "google_oauth/document"
+  autoload :Contancts, "google_oauth/contacts"
+  autoload :Mail, "google_oauth/mail"
 end
