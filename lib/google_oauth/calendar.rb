2bb26e94d86e7481c0ffb9a3f2f3687999fd11b3
Kang Chen <kang@twitter.com>
2012-06-21 22:55:07 -0700

    fixed more load paths

--- lib/google_oauth/calendar.rb
+++ lib/google_oauth/calendar.rb
@@ -1,10 +1,10 @@
 module GoogleOAuth
   module Calendar
-    autoload :Acls, "calendar/acls"
-    autoload :Calendars, "calendar/calendars"
-    autoload :Colors, "calendar/colors"
-    autoload :Events, "calendar/events"
-    autoload :FreeBusy, "calendar/free_busy"
-    autoload :Settings, "calendar/settings"
+    autoload :Acls, "google_oauth/calendar/acls"
+    autoload :Calendars, "google_oauth/calendar/calendars"
+    autoload :Colors, "google_oauth/calendar/colors"
+    autoload :Events, "google_oauth/calendar/events"
+    autoload :FreeBusy, "google_oauth/calendar/free_busy"
+    autoload :Settings, "google_oauth/calendar/settings"
   end
 end
