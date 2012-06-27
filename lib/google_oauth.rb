d7d5740cf6dd6ac2f9da7266a261a436b36acf79
Kang Chen <kang@twitter.com>
2012-06-26 20:50:57 -0700

    got google client set up

--- lib/google_oauth.rb
+++ lib/google_oauth.rb
@@ -1,12 +1,12 @@
-require "google_oauth/calendar"
+# require "google_oauth/calendar"
 require "google_oauth/client"
-require "google_oauth/document"
-require "google_oauth/contacts"
-require "google_oauth/mail"
+# require "google_oauth/document"
+# require "google_oauth/contacts"
+# require "google_oauth/mail"
 
 module GoogleOAuth
   class << self
-    attr_accessor :token, :secret, :scope, :host
+    attr_accessor :client_id, :client_secret, :scope, :host
 
     def configure
       yield self
--- lib/google_oauth/client.rb
+++ lib/google_oauth/client.rb
@@ -1,23 +1,28 @@
+require 'google_oauth/calendar'
+
 module GoogleOAuth
   class Client
-    attr_accessor :refresh_token, :client_id, :client_secret, :google_client
+    include Calendar
+
+    attr_accessor :refresh_token, :access_token, :google_client
 
     def initialize(options = {})
-      [:refresh_token, :client_id, :client_secret].each do |attr|
+      [:refresh_token, :access_token, :google_client].each do |attr|
         instance_variable_set("@#{attr}".to_sym, options[attr])
       end
 
       setup_google_client!
     end
 
-    def setup_google_client!
+    private
 
+    def setup_google_client!
       google_client = Google::APIClient.new
-      # google_client.authorization.client_id      = opts[:client_id]
-      # google_client.authorization.client_secret  = opts[:client_secret]
-      # google_client.authorization.scope          = opts[:scope]
-      # google_client.authorization.refresh_token  = opts[:refresh_token]
-      # google_client.authorization.access_token   = opts[:access_token]
+      google_client.authorization.client_id      = client_id
+      google_client.authorization.client_secret  = client_secret
+      google_client.authorization.scope          = GoogleOAuth.scope
+      google_client.authorization.refresh_token  = refresh_token
+      google_client.authorization.access_token   = access_token
 
       # user = opts[:user]
 
@@ -35,15 +40,15 @@ module GoogleOAuth
       # @client = client
     end
     
-    def self.new_from_user(user)
-      new({ :access_token => user.access_token, :refresh_token => user.refresh_token, :user => user })
-    end
+    # def self.new_from_user(user)
+    #   new({ :access_token => user.access_token, :refresh_token => user.refresh_token, :user => user })
+    # end
 
-    def expired?
-      @client.expired?
+    # def expired?
+    #   @client.expired?
 
-    def service
-      @service ||= google_client.discovered_api('calendar', 'v3')
-    end
+    # def service
+    #   @service ||= google_client.discovered_api('calendar', 'v3')
+    # end
   end
 end
